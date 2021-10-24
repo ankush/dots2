e4455f16f6ee6de242c682172643b058606fd296
Ankush Menat <ankush@frappe.io>
2021-10-24 12:26:24 +0530

    fix: completion mappings

--- .config/nvim/luainit.lua
+++ .config/nvim/luainit.lua
@@ -51,7 +51,8 @@ cmp.setup({
     mapping = {
       ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
       ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-
+      ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+      ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
       ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true})
     },
     sources = {
